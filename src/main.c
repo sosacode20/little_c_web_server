300109cdab11b52e3895ecd7ea8a326cc423c05c
dfg-98 <dariofg98@gmail.com>
2022-01-30 02:08:46 -0500

    refactor some code and add function for handle requests

--- src/main.c
+++ src/main.c
@@ -1,4 +1,135 @@
 #include "common.h"
+#include "meta.h"
+#include "robust_io.h"
+
+char WEBSERVER_ROOT[256];
+
+void serve(int fd);
+
+int open_listenfd(int port);
+
+void sigchld_handler(int sig);
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd, port;
+
+    typedef unsigned int socklen_t;
+    socklen_t clientlen;
+    struct sockaddr_in clientaddr;
+    struct hostent *host_p;
+    char *haddrp;
+    char str[100];
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s  port webserver-root\n", argv[0]);
+        exit(0);
+    }
+
+    port = atoi(argv[1]);
+    strcpy(WEBSERVER_ROOT, argv[2]);
+
+    signal(SIGCHLD, sigchld_handler);
+    listenfd = open_listenfd(port);
+
+    fprintf(stdout, "Servidor corriendo en el puerto %d\n", port);
+    fprintf(stdout, "Raiz del servidor %s\n", WEBSERVER_ROOT);
+
+    while (1)
+    {
+        clientlen = sizeof(clientaddr);
+        connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+        host_p = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        haddrp = inet_ntoa(clientaddr.sin_addr);
+        printf("Connected to %s (%s)\n", host_p->h_name, haddrp);
+        if (fork() == 0)
+        {
+            close(listenfd);
+            serve(connfd);
+            // write(connfd, str, strlen(str)+1);
+            // send_response(connfd);
+            close(connfd);
+            exit(0);
+        }
+        close(connfd);
+    }
+}
+
+void read_requesthdrs(rio_t *rp)
+{
+    char buf[MAXLINE];
+
+    rio_readline(rp, buf, MAXLINE);
+
+    while (strcmp(buf, "\r\n"))
+    {
+        rio_readline(rp, buf, MAXLINE);
+        printf("%s", buf);
+    }
+    return;
+}
+
+int is_dir(char *uri)
+{
+
+    file_info fi;
+    if (get_file_info(uri, &fi) == 0)
+    {
+        if (strcmp(fi.file_type, "directory") == 0)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void serve(int fd)
+{
+    int is_directory;
+    struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char filename[MAXLINE];
+    rio_t rio;
+
+    init_rio_buf(&rio, fd);
+    rio_readline(&rio, buf, MAXLINE);
+    sscanf(buf, "%s %s %s", method, uri, version);
+    printf("%s %s %s\n", method, uri, version);
+
+    if (strcasecmp(method, "GET"))
+    {
+        clienterror(fd, method, "501", "Not Implemented",
+                    "This Webserver only implements GET method");
+        return;
+    }
+    read_requesthdrs(&rio);
+
+    strcpy(filename, WEBSERVER_ROOT);
+    strcat(filename, uri);
+    is_directory = is_dir(filename);
+
+    if (stat(filename, &sbuf) < 0)
+    {
+        clienterror(fd, filename, "404", "Not found",
+                    "Couldn’t find this file");
+        return;
+    }
+
+    if (!is_directory)
+    {
+        if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode))
+        {
+            clienterror(fd, filename, "403", "Forbidden", "Couldn’t read the file");
+            return;
+        }
+        serve_static(fd, filename, sbuf.st_size);
+    }
+    else
+    {
+        serve_dynamic(fd, filename);
+    }
+}
 
 int open_listenfd(int port)
 {
@@ -37,61 +168,5 @@ void sigchld_handler(int sig)
 {
     while (waitpid(-1, 0, WNOHANG) > 0)
     {
-    }
-}
-
-void send_response(int connfd)
-{
-    write(connfd, "HTTP/1.1 200 OK\n", 16);
-    write(connfd, "Content-length: 70\n", 19);
-    write(connfd, "Content-Type: text/html\n\n", 25);
-    write(connfd, "<html><body><h1>Bienvenido a nuestro servidor hecho en C</h1></body></html>", 70);
-}
-
-int main(int argc, char **argv)
-{
-    int listenfd, connfd, port;
-    char webserver_root[256];
-    typedef unsigned int socklen_t;
-    socklen_t clientlen;
-    struct sockaddr_in clientaddr;
-    struct hostent *host_p;
-    char *haddrp;
-    char str[100];
-
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s  port webserver-root\n", argv[0]);
-        exit(0);
-    }
-
-    port = atoi(argv[1]);
-    strcpy(webserver_root, argv[2]);
-
-    signal(SIGCHLD, sigchld_handler);
-    listenfd = open_listenfd(port);
-
-    fprintf(stdout, "Servidor corriendo en el puerto %d\n", port);
-    fprintf(stdout, "Raiz del servidor %s\n", webserver_root);
-
-    while (1)
-    {
-        clientlen = sizeof(clientaddr);
-        connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
-        host_p = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-        haddrp = inet_ntoa(clientaddr.sin_addr);
-        printf("Connected to %s (%s)\n", host_p->h_name, haddrp);
-        if (fork() == 0)
-        {
-            close(listenfd);
-            bzero(str, 100);
-            read(connfd, str, 100);
-            printf("Echoing back - %s", str);
-            // write(connfd, str, strlen(str)+1);
-            send_response(connfd);
-            close(connfd);
-            exit(0);
-        }
-        close(connfd);
     }
 }
