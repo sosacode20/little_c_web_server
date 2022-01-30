0d75a251769182b4b54ffd9345ecc3c353bc1948
dfg-98 <dariofg98@gmail.com>
2022-01-30 02:06:53 -0500

    build: add common libs

--- include/common.h
+++ include/common.h
@@ -16,5 +16,22 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <signal.h>
+#include <sys/wait.h>
+#include <sys/errno.h>
+#include <sys/mman.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <strings.h>
+#include <unistd.h>
+#include <dirent.h>
+
+#ifndef MAXLINE
+#define MAXLINE 4096
+#endif
+
+#ifndef MAXBUF
+#define MAXBUF 64536
+#endif
 
 #endif
